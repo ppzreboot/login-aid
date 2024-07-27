8078e73bd7c972d52d7d89c81e835fc33c7a29fb
ppzreboot <ppzreboot@qq.com>
2024-07-27 13:46:46 +0000

    docs: ...

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # login-aid.ts
-login aid with:
+login with:
 + [github](https://jsr.io/@ppz/login-aid@latest/doc/~/Login_aid_github) (unavailable from china)
 
+It likes:
 ``` ts
 import { Login_aid_github } from '@ppz/login-aid'
 
 // You need a Login_aid_github instance.
 const with_github = new Login_aid_github(github_client_id, github_client_secret)
 
-// And exchange userinfo with code.
+// And exchange code for userinfo.
 const userinfo = await with_github.login(code)
 ```
 
-[Document](https://jsr.io/@ppz/login-aid/doc)
+[API Document](https://jsr.io/@ppz/login-aid/doc)
 | [demo for deno](https://github.com/ppzreboot/login-aid.ts/tree/main/demo/deno)
--- mod.ts
+++ mod.ts
@@ -1,14 +1,2 @@
-/**
- * @module
- * ### Login with github
- * 1. Create your github APP [here](https://github.com/settings/apps) and generate your client id and client secret.
- * 2. In your APP, direct the user to https://github.com/login/oauth/authorize, and add client id as a query parameter. For example: https://github.com/login/oauth/authorize?client_id=12345.
- * 3. If the user accepts your authorization request, GitHub will redirect the user to one of the callback URLs in your app settings, and provide a `code` query parameter.
- * 4. Your server will receive a request (redirection at previous step) with a `code` query parameter.
- * 5. In your server, call `Login_aid_github::login(code)`, and you will get userinfo.
- * 
- * Details on [login with github](https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-user-access-token-for-a-github-app#using-the-device-flow-to-generate-a-user-access-tokehttps://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-user-access-token-for-a-github-app).
- */
-
 export * from './with-github.ts'
 export * from './error.ts'
